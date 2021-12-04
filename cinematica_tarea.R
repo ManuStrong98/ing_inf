675dbc8b379a7f1d0e61e2e65f19104929b494e1
Manu_ls_learning <josemanueldj2011@hotmail.com>
2021-12-04 14:36:43 -0400

    creo_que_original

--- cinematica_tarea.R
+++ cinematica_tarea.R
@@ -27,29 +27,17 @@
    
    # MC MRUV
       
-      f = lm(c$MRUV~poly(x = c$t, degree = 2))
-      f = lm(c$MRUV~c$t)
-   f = lm(c$MRU~c$t)
-   f = lm(c$MRU~c$t)
-   f = lm(c$MRU~c$t)
-   g = lm(c$MRUV~c$t)
-   plot(f)
-   lines(c$t, predict(f))
-   summary(f)
-   
-   abline(f, col = "red")
-   abline(g)
-
-   
-   ################ MC
-   z = (c$t)^2
-   y = c$MRUV
-   a_m = (sum(y) * sum(z^2) - sum(z*y) * sum(z)) / (
-      length(y) * sum(z^2) - (sum(z))^2)
-   
-   b_m = (length(y) * sum(z*y) - sum(z) * sum(y)) / (
-      (length(y) * sum(z^2) - (sum(z))^2)   )
-   yrecta = a_m + b_m * z    
-   #plot(c$t, yrecta)
-   lines(c$t, yrecta, col= "BLUE")
-plot(c$t, yrecta)   
+      
+      z = (c$t)^2
+      y = c$MRUV
+      
+      a_m = (sum(y) * sum(z^2) - sum(z*y) * sum(z)) / (
+         length(y) * sum(z^2) - (sum(z))^2)
+      
+      b_m = (length(y) * sum(z*y) - sum(z) * sum(y)) / (
+         (length(y) * sum(z^2) - (sum(z))^2)   )
+      yrecta = a_m + b_m * z    
+      
+      
+      lines(c$t, yrecta, col= "BLUE")
+      
